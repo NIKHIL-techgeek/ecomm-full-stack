37450c6efc7fdafbc909c66ba2aa44e4f4599fc0
Nikhil Kapoor <kapoornikhil.in@gmail.com>
2023-06-01 15:49:30 -0400

    password hashing

---
+++ helper/authHelper.js
@@ -0,0 +1,21 @@
+//  first func for hash and second for decrpt , compare
+
+import  hash  from "bcrypt";
+import bcrypt from bcrypt;
+
+export const hashPassword=async(password)=>{
+    try
+    {
+        const saltRounds=10
+        const hashedPassword=await bcrypt.hash(password,saltRounds);
+        return hashedPassword
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+};
+
+export const comparePassword= async(password,hashedPassword)=>{
+    return bcrypt.compare(password,hashedPassword);
+};
