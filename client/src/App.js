5d48dd0c3f3fafac320afe64a29694e679e95157
Nikhil Kapoor <kapoornikhil.in@gmail.com>
2023-06-08 15:36:38 -0400

    Router dom Installed

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
-import "./App.css";
-
-import Layout from './Components/Layout/Layout.js'
-
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Policy from './pages/Policy';
+import Pagenotfound from './pages/Pagenotfound';
 function App() {
-  return (
+  return(
     <>
-      <Layout>
-        <h1>Ecommerce App</h1>
-      </Layout>
-
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/policy" element={<Policy/>}/>
+      <Route path="*" element={<Pagenotfound/>}/>
+    </Routes>
     </>
   );
 }
---
+++ client/src/pages/Pagenotfound.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../Components/Layout/Layout";
+
+const Pagenotfound = () => {
+  return (
+    <Layout>
+      <h1>Pagenotfound</h1>
+    </Layout>
+  );
+};
+
+export default Pagenotfound;
