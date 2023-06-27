5fb034c34f178f8a4785dca224276c25c59c98cb
Nikhil Kapoor <kapoornikhil.in@gmail.com>
2023-06-27 14:12:05 -0400

    resetting nav bar

--- client/src/Components/Layout/Header.js
+++ client/src/Components/Layout/Header.js
@@ -17,47 +17,30 @@ const Header = () => {
             <span className="navbar-toggler-icon" />
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <NavLink to="/" className="navbar-brand" href="#">
-              ECOMMERCE
+            <NavLink to="/" className="navbar-brand">
+              Hidden brand
             </NavLink>
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <NavLink
-                  to="/"
-                  className="nav-link "
-         
-
-                >
+                <NavLink to="/" className="nav-link" aria-current="page">
                   Home
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink
-                  to="/category"
-                  className="nav-link"
-                
-                >
-                  Category
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/Register" className="nav-link">
+                <NavLink to="/register" className="nav-link">
                   Register
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="/Login" className="nav-link" >
-                  LogIn
+                <NavLink to="/login" className="nav-link">
+                  Login
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="/Cart" className="nav-link" >
-                  Cart {"("}
-                  {0}
-                  {")"}
+                <NavLink to="/cart" className="nav-link">
+                  Cart (0)
                 </NavLink>
               </li>
-              <li className="nav-item"></li>
             </ul>
           </div>
         </div>
