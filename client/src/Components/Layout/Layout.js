68ef1132861f2e2a7379b67de94d0a337880dcb0
Nikhil Kapoor <kapoornikhil.in@gmail.com>
2023-06-27 16:07:42 -0400

    working on seo

--- client/src/Components/Layout/Layout.js
+++ client/src/Components/Layout/Layout.js
@@ -1,12 +1,22 @@
 import React from "react";
-import Header from "./Header.js"
-import Footer from "./Footer.js"
-const Layout = (props) => {
+import Header from "./Header.js";
+import Footer from "./Footer.js";
+import Helmet from "react-helmet";
+const Layout = (props, title, keywords, author) => {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+
+        <meta name="description" content="Free Web tutorials" />
+        <meta name="keywords" content="HTML, CSS, JavaScript" />
+        <meta name="author" content="John Doe" />
+
+        <title>My Title</title>
+      </Helmet>
       <Header />
-      <main style={{minHeight:'70vh'}}>{props.children}</main>
-      <Footer/>
+      <main style={{ minHeight: "70vh" }}>{props.children}</main>
+      <Footer />
     </div>
   );
 };
