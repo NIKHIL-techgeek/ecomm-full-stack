44a2f0d7bcdcf50f9f2256c3cf90a3e8eda3523a
Nikhil Kapoor <kapoornikhil.in@gmail.com>
2023-06-30 14:14:56 -0400

    started work on register

---
+++ client/src/pages/Auth/Register.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../Components/Layout/Layout'
+
+const Register = () => {
+  return (
+    <Layout title="Register -- Ecommer App">
+        <h1>Register Page</h1>
+    </Layout>
+  )
+}
+
+export default Register
