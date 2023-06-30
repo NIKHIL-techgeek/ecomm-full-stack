2e2fbc704d07cad92df514e8d7ac2fba1ee58b3c
Nikhil Kapoor <kapoornikhil.in@gmail.com>
2023-06-30 15:03:29 -0400

    register form

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,12 +1,71 @@
-import React from 'react'
-import Layout from '../../Components/Layout/Layout'
+import React,{useState} from "react";
+import Layout from "../../Components/Layout/Layout";
 
 const Register = () => {
+    const [name,setName]= useState("")
+    const [email,setemail]= useState("")
+    const [password,setpassword]= useState("")
+    const [phone,setphone]= useState("")
+    const [address,setaddress]= useState("")
   return (
     <Layout title="Register -- Ecommer App">
+      <div className="register">
         <h1>Register Page</h1>
-    </Layout>
-  )
-}
+        <form>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={name}
+              onChange={(e)=>setName(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter Your Name"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="email"
+              value={email}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter Your Email"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              value={password}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter Your Password"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={phone}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter Your Phone No"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={address}
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter Your Address"
+            />
+          </div>
 
-export default Register
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </Layout>
+  );
+};
+
+export default Register;
--- client/src/pages/Pagenotfound.js
+++ client/src/pages/Pagenotfound.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "../Components/Layout/Layout";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Pagenotfound = () => {
   return (
