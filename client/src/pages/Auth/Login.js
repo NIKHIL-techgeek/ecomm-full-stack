0b09bcf8e4ffedc403153e737a2a0f156f59b1ad
Nikhil Kapoor <kapoornikhil.in@gmail.com>
2023-07-09 00:20:53 -0400

    working on local storage

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -23,6 +23,7 @@ const Login = () => {
       if (res && res.data.success) {
         toast.success(res.data && res.data.message);
         setAuth({...auth,user:res.data.user,token:res.data.token})
+        localStorage.setItem('auth',JSON.stringify(res.data));
         navigate("/");
       } else {
         toast.error(res.data.message);
