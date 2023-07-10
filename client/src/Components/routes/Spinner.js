665e9efe0082b8e094cae6ee2184b306bf83d046
Nikhil Kapoor <kapoornikhil.in@gmail.com>
2023-07-09 21:36:42 -0400

    private route for dashboard working

--- client/src/Components/routes/Spinner.js
+++ client/src/Components/routes/Spinner.js
@@ -1,9 +1,23 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 const Spinner = () => {
+  const [count, setCount] = useState(5);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prevVal) => --prevVal);
+    }, 1000);
+    count === 0 && navigate("/login");
+    return () => clearInterval(interval);
+  }, [count, navigate]);
   return (
     <>
-      <div className="d-flex justify-content-center">
+      <div
+        className="d-flex flex-column justify-content-center align-items-center"
+        style={{ height: "100vh" }}
+      >
+        <h1 className="Text-center">Redirecting to you in {count} second</h1>
         <div className="spinner-border" role="status">
           <span className="visually-hidden">Loading...</span>
         </div>
