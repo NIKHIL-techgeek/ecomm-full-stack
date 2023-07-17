dd924fa6d78b6155c442b6a49508ef3fae07d8c1
Nikhil Kapoor <kapoornikhil.in@gmail.com>
2023-07-17 13:33:43 -0400

    manage category front end working

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -1,19 +1,27 @@
-import React ,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../Components/Layout/Layout";
 import AdminMenu from "../../Components/Layout/AdminMenu";
 import toast from "react-hot-toast";
+import axios from "axios";
 const CreateCategory = () => {
-  const [category,setCategory]=useState([]);
+  const [category, setCategory] = useState([]);
 
   // get all category
-  const getAllCategory=async ()=>{
+  const getAllCategory = async () => {
     try {
-      
+      const { data } = await axios.get('/api/v1/category/get-category');
+      if (data.success) {
+        setCategory(data.category);
+      }
     } catch (error) {
-      console.log(error); 
-      toast.error("Something went wrong in getting category")
+      console.log(error);
+      toast.error("Something went wrong in getting catgeory");
     }
-  }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, []);
   return (
     <Layout title={"Dashboard-Create Category"}>
       <div className="container-fluid m-3 p-3">
@@ -22,7 +30,24 @@ const CreateCategory = () => {
             <AdminMenu />
           </div>
           <div className="col-md-9">
-            <h1>Create Category</h1>
+            <h1>Manage Category</h1>
+            <div>
+              <table classname="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    {category.map(c => (
+                      <td key={c._id}>{c.name}</td>
+                    ))}
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
